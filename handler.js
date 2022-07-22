675ea28a3f88db7f5d12aaa24e8fb24065f64f7a
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-07-22 22:33:00 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -32,12 +32,12 @@ module.exports = {
         global.author = global.data.owner
         //global.author = '𝐉𝐚𝐫𝐨𝐭'
         global.wm2 = global.data.namabot + ' ' + global.data.owner
-        global.wm = '© 𝐀𝐥𝐩𝐢 𝐁𝐨𝐭'
+        global.wm = '© 𝗗𝗮𝗿𝗸 𝐁𝐨𝐭'
         global.colong1 = 'Ciss 📸'
-        global.colong2 = '𝐀𝐥𝐩𝐢 𝑩𝒐𝒕'
+        global.colong2 = '𝗗𝗮𝗿𝗸 𝑩𝒐𝒕'
         global.kontak2 = [
         ['6285850539404', '𝐉𝐚𝐫𝐨𝐭', '𝑫𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓 𝑩𝒐𝒕', 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ', true],
-        ['6285713137903', await this.getName('6285713137903@s.whatsapp.net'), '𝑷𝒂𝒓𝒕𝒏𝒆𝒓 𝐀𝐥𝐩𝐢 𝑩𝒐𝒕', 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ', true], 
+        ['62858505394047', await this.getName('62858505394047@s.whatsapp.net'), '𝑷𝒂𝒓𝒕𝒏𝒆𝒓 𝗗𝗮𝗿𝗸 𝑩𝒐𝒕', 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ', true], 
         ]
         global.bg = await (await fetch(img)).buffer()
         global.time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
